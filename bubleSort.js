88884845c3d7bb08761573c4c7e3ef4c833008a3
Hovhannes <hovhannes.sisoyan@betconstruct.am>
2020-02-21 13:00:25 +0400

    bubleSort completed

--- bubleSort.js
+++ bubleSort.js
@@ -4,7 +4,7 @@ const a = [5,1,2,8,7,4,9,3,6,0, -10000, 0,0,0, 5404];
 
 const bubleSort = (array) => {
     let lastSortedIndex = array.length - 2;
-    while (lastSortedIndex > -1) {
+    while (lastSortedIndex >= 0) {
         for (let i=0; i<=lastSortedIndex; i++){
             if (array[i] > array[i+1]) {
                 swap(array, i, i+1);
